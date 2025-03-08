05586aa654966d1cdebf95cd83243ec34b8635ea
Trevor Evans <trevalexandro@gmail.com>
2025-03-07 22:32:55 -0600

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import FinalThemeProvider from "./components/theme-provider";
 import DisplayMode from "./components/display-mode";
 import { ReactNode } from "react";
+import {Analytics} from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: "Trevor Evans Portfolio",
@@ -20,6 +21,7 @@ const RootLayout = ({ children }: RootLayoutProps) => {
         <FinalThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <DisplayMode className="absolute top-20 right-20 z-10" />
           {children}
+          <Analytics />
         </FinalThemeProvider>
       </body>
     </html>
